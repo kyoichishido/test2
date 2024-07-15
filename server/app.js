ca4b5b04affd220bd95f0a9dfe3358ab65141183
jayraj singh <54437859+kyoichishido@users.noreply.github.com>
2024-07-15 14:57:06 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan('dev'));
 app.use(cors({
-    origin: 'https://lms-system-omega.vercel.app',
+    origin: '*',
     credentials: true
 }));
 app.use(morgan('dev'))
